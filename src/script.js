4007a2c5d38aab70b911f66e026bef8cdcda566b
jlangley3 <jlangleyiii@aol.com>
2020-06-15 17:28:27 -0400

    hidding timer, auto update

--- src/script.js
+++ src/script.js
@@ -3,21 +3,46 @@ const Ex_URL = `${URL}/exercises`
 const Work_URL = `${URL}/workouts`
 const Both_URL = `${URL}/exercise_workouts`
 
+let workOut = false;
+let wo_button;
+
+const timerContainer = () => document.querySelector("#wotimer");
+const workOutCard = () => document.querySelector("#selected");
+
 
 document.addEventListener("DOMContentLoaded", function() {
     console.log("page is loaded");
+    wo_button = document.querySelector("#wobutton");
+    let submit = document.querySelector(".submit");
     fetchExercises();
     fetchWorkouts();
     timerTech();
-    let submit = document.querySelector(".submit");
-    let wo_button = document.querySelector("#wobutton");
+    timerContainer().style.display = "none";
+    // workOutCard().style.display = "none";
+    // let workOutCard = document.querySelector("#selected");
     submit.addEventListener("click", (event) => {
         createWorkout(event)
     });
     wo_button.addEventListener("click", (event) => {
-        startWorkout(event)
+        startWorkout(event);
+        toggleWoDisplay();
+        if (wo_button.innerText === "START") {
+            wo_button.innerText = "QUIT?";
+        } else {
+            wo_button.innerText = "START"
+        }
+
     });
 });
+
+function toggleWoDisplay() {
+    workOut = !workOut
+    if (workOut) {
+        timerContainer().style.display = "block"
+    } else {
+        timerContainer().style.display = "none"
+    }
+}
 
 function fetchExercises() {
     fetch(Ex_URL)
@@ -129,12 +154,14 @@ function createWorkout(event) {
 }
 
 function showWorkout(event, wo) {
+    workOutCard().querySelector("h3").innerText = "";
+    workOutCard().querySelector("h3").innerText = wo.name;
     let div = document.querySelector("#selected");
     let ol = document.querySelector(".ex");
     ol.innerHTML = "";
     let h = document.querySelector(".wo");
     h.innerHTML = "";
-    h.innerText = wo.name;
+    // h.innerText = wo.name;
 
     let p = document.createElement("p");
     let p1 = document.createElement("p");
@@ -145,9 +172,9 @@ function showWorkout(event, wo) {
     p.dataset.on = wo.work_time;;
     p2.style.color = "#b1fa07";
     p.dataset.rest = wo.rest_time;
-    p.innerText = "Rounds:" + " " + wo.rounds;
-    p1.innerText = "On:" + " " + wo.work_time;
-    p2.innerText = "Rest:" + " " + wo.rest_time;
+    p.innerText = wo.rounds + " " + "Rounds";
+    p1.innerText = wo.work_time + " " + " Second Intervals";
+    p2.innerText = wo.rest_time + " " + "Second Rest Perods";
     wo.exercises.forEach(function(ex) {
         let li2 = document.createElement("li");
         li2.innerText = ex.name;
@@ -170,6 +197,7 @@ function startWorkout(event) {
     onTime.value = workTime.dataset.on;
     offTime.value = restTime.dataset.rest;
     intervals.value = rounds.dataset.rounds;
+    document.querySelector("#update").click();
 
 }
 // }
@@ -179,6 +207,9 @@ function timerTech() {
     let seconds = 20;
     let rest = true;
     let interval;
+    let round;
+    let totalTime;
+    let oneTime;
 
     let intervalTime = 20;
     let breakTime = 10;
@@ -189,6 +220,7 @@ function timerTech() {
     let intervalInput = document.getElementById("intervalTime");
     let breakInput = document.getElementById("breakTime");
     let roundsInput = document.getElementById("intervals");
+    let show = document.querySelector("#intervals");
 
     let startButton = document.getElementById("start");
     let pauseButton = document.getElementById("pause");
@@ -203,14 +235,27 @@ function timerTech() {
         intervalTime = Math.floor(intervalInput.value * 1);
         breakTime = Math.floor(breakInput.value * 1);
         rounds = Math.floor(roundsInput.value * 1);
+        oneRoundTime = intervalTime + breakTime + 1;
         reset();
         changeToGo();
     }
+
+    // wo_button.onclick = function() {
+    //     intervalTime = Math.floor(intervalInput.value * 1);
+    //     breakTime = Math.floor(breakInput.value * 1);
+    //     rounds = Math.floor(roundsInput.value * 1);
+    //     oneRoundTime = intervalTime + breakTime + 1;
+    //     reset();
+    //     changeToGo();
+    // }
 
     startButton.onclick = function() {
         rest = false;
         changeToGo();
         interval = setInterval(countdownSeconds, 1000);
+        totalTime = (oneRoundTime * 1000) * rounds;
+        round = setTimeout(roundTimeOut, totalTime);
+        oneTime = setInterval(showRoundsLeft, (oneRoundTime * 1000));
     }
 
     function countdownSeconds() {
@@ -219,14 +264,24 @@ function timerTech() {
         checkForStateChange();
     }
 
-    resetButton.onclick = function() {
+    function roundTimeOut() {
+        alert("Great Workout!");
         reset();
-
     }
 
+    function showRoundsLeft() {
+        // let show = document.querySelector("#intervals");
+        roundsInput.value = --rounds;
+    }
+
+    resetButton.onclick = function() {
+        reset();
+    }
 
     function reset() {
         clearInterval(interval);
+        clearInterval(round);
+        clearInterval(oneTime);
         seconds = intervalTime;
         secondsSpan.innerText = seconds;
         rest = false;
